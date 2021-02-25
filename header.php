f638d892356f5f66cae0c22a1245e6a99b929ace
Paulo Henrique <79285536+pauloboare@users.noreply.github.com>
2021-02-24 23:38:47 -0300

    commit initial
    very early

---
+++ header.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>twriter</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+<div id="container" class="fullheight">
+<header role="banner" class="fullheight">
+    <nav role="navigation">
+
+    <a href="#" role="link"><div><span>Página Inicial</span></div></a>
+    <a href="#" role="link"><div><span>Explorar</span></div></a>
+    <a href="#" role="link"><div><span>Notificações</span></div></a>
+    <a href="#" role="link"><div><span>Mensagens</span></div></a>
+    <a href="#" role="link"><div><span>Itens Salvos</span></div></a>
+    <a href="#" role="link"><div><span>Listas</span></div></a>
+    <a href="#" role="link"><div><span>Perfil</span></div></a>
+    <a href="#" role="link"><div><span>Mais</span></div></a>
+
+    </nav>
+
+    <div class="twreetar">
+        <a href="#" role="button"> <span>Twreetar</span></a>
+    </div>
+</header>
+    
---
+++ index.php
@@ -0,0 +1,11 @@
+<?php include "header.php"; ?>
+<main role="main" class="fullheight">
+feed
+</main>
+
+
+
+
+</div>
+</body>
+</html>

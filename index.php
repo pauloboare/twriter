f89e046f5e830451ea7ece9eeb6c1a314aeb20d7
Paulo Henrique <79285536+pauloboare@users.noreply.github.com>
2021-02-26 00:34:10 -0300

    chronological feed

--- index.php
+++ index.php
@@ -11,7 +11,7 @@
         <div id="feed">
             <div id="feed-form">
                 <form method="post">
-                    <textarea name="post"></textarea>
+                    <textarea name="post" id="twretar" placeholder="O que está acontecendo?"></textarea>
                     <div class="twreetar">
                         <input type="submit" value="Twreetar">
                     </div>
@@ -21,7 +21,7 @@
                     if(isset($_POST['post'])){
                     $a=$_SESSION['id-user'];
                     $b=$_POST['post'];
-                    $sql="INSERT INTO tb_posts VALUES (NULL, $a,'$b', '')";
+                    $sql="INSERT INTO tb_posts VALUES (NULL, '$a','$b', NOW())";
                     $result=mysqli_query($connect, $sql);
                     if ($result) {
                         header('location:index.php');;
@@ -37,32 +37,57 @@
               
             </div>
             <section id="feed-posts">
-                <article class="posts">
+                
 
                 <?php
+                date_default_timezone_set('America/Sao_Paulo');
                 include "connect.php";
-                $sql = mysqli_query($connect, "SELECT * FROM tb_posts INNER JOIN tb_users ON tb_posts.fk_user = tb_users.id_users");
+                $sql = mysqli_query($connect, "SELECT * FROM tb_posts INNER JOIN tb_users ON tb_posts.fk_user = tb_users.id_users ORDER BY datatime DESC");
                 while ($result=mysqli_fetch_array($sql)) {
-                    echo "@".$result['username']." | ".$result['name'] ;
-                    echo "<br>";
-                    echo $result['post'];
-                    echo "<hr>";
+
+                    $dataStart = $result['datatime'];
+                    $dataNow = date("Y-m-d H:i:s");
+
+                    $unix_Start = strtotime($dataStart);
+                    $unix_Now = strtotime($dataNow);
+
+                    $hours    = floor(($unix_Now - $unix_Start) / 3600);
+                    $minutes  = floor((($unix_Now - $unix_Start) - ($hours * 3600)) / 60);
+
+                    if($hours<1){
+                        $crhonology= round($minutes,0);
+                        $typeTime=" min";
+                    }
+                    elseif($hours>=1){
+                        $crhonology= round($hours,0);
+                        $typeTime=" h";
+                    }
+
+                    echo "<article id='posts'>
+                    <div id='user-avatar'>
+                        <img src='img/default.png' id='img-avatar'>
+                    </div>
+                    <div id='post-single'> @".
+                    $result['username']." ".$result['name']." - " .$crhonology. " ".$typeTime. "<br>".
+                    $result['post'].
+                    "</div>
+                    </article>";
                 }
                 ?>
 
 
 
-                </article>
+                
             </section>
         </div>
     </div>
+
     <aside id="feed-right">
         <div id="aside-search">
-                        <input type="text" id="search-home" placeholder="Buscar no Twriter">
+            <input type="text" id="search-home" placeholder="Buscar no Twriter">
         </div>
         <section id="trendings">
-                <h3 id="title-trendings"> O que está acontecendo </h3>
-                
+            <h3 id="title-trendings"> O que está acontecendo </h3>      
         </section>
     </aside>
 
--- sql/db_twriter.sql
+++ sql/db_twriter.sql
@@ -1,23 +1,24 @@
 -- phpMyAdmin SQL Dump
--- version 4.3.11
--- http://www.phpmyadmin.net
+-- version 5.0.2
+-- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: 25-Fev-2021 às 15:29
--- Versão do servidor: 5.6.24
--- PHP Version: 5.6.8
+-- Tempo de geração: 26-Fev-2021 às 04:30
+-- Versão do servidor: 10.4.11-MariaDB
+-- versão do PHP: 7.4.6
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
 SET time_zone = "+00:00";
 
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
+/*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `db_twriter`
+-- Banco de dados: `db_twriter`
 --
 
 -- --------------------------------------------------------
@@ -26,7 +27,7 @@ SET time_zone = "+00:00";
 -- Estrutura da tabela `tb_followers`
 --
 
-CREATE TABLE IF NOT EXISTS `tb_followers` (
+CREATE TABLE `tb_followers` (
   `id_followers` int(6) NOT NULL,
   `fk_user_follower` int(6) NOT NULL,
   `fk_user_following` int(6) NOT NULL
@@ -38,21 +39,21 @@ CREATE TABLE IF NOT EXISTS `tb_followers` (
 -- Estrutura da tabela `tb_posts`
 --
 
-CREATE TABLE IF NOT EXISTS `tb_posts` (
+CREATE TABLE `tb_posts` (
   `id_posts` int(6) NOT NULL,
   `fk_user` int(6) NOT NULL,
   `post` varchar(144) NOT NULL,
   `datatime` datetime NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Extraindo dados da tabela `tb_posts`
 --
 
 INSERT INTO `tb_posts` (`id_posts`, `fk_user`, `post`, `datatime`) VALUES
-(10, 4, 'hello world', '0000-00-00 00:00:00'),
-(11, 4, 'showww', '0000-00-00 00:00:00'),
-(12, 3, 'belezaaaaaaaas', '0000-00-00 00:00:00');
+(30, 4, 'hello', '2021-02-26 00:28:21'),
+(31, 5, 'aloooo', '2021-02-26 00:28:47'),
+(32, 5, 'shoooow', '2021-02-26 00:28:56');
 
 -- --------------------------------------------------------
 
@@ -60,63 +61,67 @@ INSERT INTO `tb_posts` (`id_posts`, `fk_user`, `post`, `datatime`) VALUES
 -- Estrutura da tabela `tb_users`
 --
 
-CREATE TABLE IF NOT EXISTS `tb_users` (
+CREATE TABLE `tb_users` (
   `id_users` int(6) NOT NULL,
   `username` varchar(12) NOT NULL,
-  `name` varchar(15) NOT NULL,
+  `name` varchar(32) NOT NULL,
   `password` varchar(40) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Extraindo dados da tabela `tb_users`
 --
 
 INSERT INTO `tb_users` (`id_users`, `username`, `name`, `password`) VALUES
-(2, 'webdominante', 'Paulo', '883d5554d83ebae9c26936212f20b9a2'),
 (3, 'jefferson15', 'Jefferson', '883d5554d83ebae9c26936212f20b9a2'),
-(4, 'pauloboare', 'Paulo', '883d5554d83ebae9c26936212f20b9a2');
+(4, 'pauloboare', 'Paulo Henrique', '883d5554d83ebae9c26936212f20b9a2'),
+(5, 'cine_barreto', 'Francine Barreto', '883d5554d83ebae9c26936212f20b9a2');
 
 --
--- Indexes for dumped tables
+-- Índices para tabelas despejadas
 --
 
 --
--- Indexes for table `tb_followers`
+-- Índices para tabela `tb_followers`
 --
 ALTER TABLE `tb_followers`
   ADD PRIMARY KEY (`id_followers`);
 
 --
--- Indexes for table `tb_posts`
+-- Índices para tabela `tb_posts`
 --
 ALTER TABLE `tb_posts`
   ADD PRIMARY KEY (`id_posts`);
 
 --
--- Indexes for table `tb_users`
+-- Índices para tabela `tb_users`
 --
 ALTER TABLE `tb_users`
   ADD PRIMARY KEY (`id_users`);
 
 --
--- AUTO_INCREMENT for dumped tables
+-- AUTO_INCREMENT de tabelas despejadas
 --
 
 --
--- AUTO_INCREMENT for table `tb_followers`
+-- AUTO_INCREMENT de tabela `tb_followers`
 --
 ALTER TABLE `tb_followers`
   MODIFY `id_followers` int(6) NOT NULL AUTO_INCREMENT;
+
 --
--- AUTO_INCREMENT for table `tb_posts`
+-- AUTO_INCREMENT de tabela `tb_posts`
 --
 ALTER TABLE `tb_posts`
-  MODIFY `id_posts` int(6) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=13;
+  MODIFY `id_posts` int(6) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=33;
+
 --
--- AUTO_INCREMENT for table `tb_users`
+-- AUTO_INCREMENT de tabela `tb_users`
 --
 ALTER TABLE `tb_users`
-  MODIFY `id_users` int(6) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=5;
+  MODIFY `id_users` int(6) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+COMMIT;
+
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;

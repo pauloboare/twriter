956ef69f54550c0ff6888575d26a074cdbf38902
Paulo Henrique <79285536+pauloboare@users.noreply.github.com>
2021-03-03 15:35:22 -0300

    Update modal.php

--- modal.php
+++ modal.php
@@ -1,10 +1,46 @@
 <!-- Modal HTML embedded directly into document -->
 <div id="ex1" class="modal">
-  <p><?php 
  
-     include "form-post.php";
  
-     ?></p>
+  <p>
+  <div id="feed-form">
+                    <div id='form-avatar'>
+                        <img src='img/default.png' id='img-avatar'>
+                    </div>
+                    <div id='mypost'>
+                        <form method="post">
+                            <textarea name="mpost" id="twretar" placeholder="O que está acontecendo?"></textarea>
+                            <div class="twreetar">
+                                <div id="btns-forms">
+                                    <div id="btns-left">
+                                       <span class="material-icons btns-left">insert_photo</span>
+                                       <span class="material-icons btns-left">gif</span>
+                                       <span class="material-icons btns-left">poll</span>
+                                       <span class="material-icons btns-left">mood</span>
+                                       <span class="material-icons btns-left">schedule_send</span>
+                                    </div>
+                                    <div id="btns-right">
+                                        <input type="submit" id="btn-post" value="Twreetar" onClick='window.location.reload()'>
+                                    </div>
+                                </div>
+                            </div>
+                            <?php 
+                            include "connect.php";
+                           
+                            if(isset($_POST['mpost'])){
+                            $ma=$_SESSION['id-user'];
+                            $mb=$_POST['mpost'];
+                            $sql="INSERT INTO tb_posts (id_posts, fk_user, post, datatime) VALUES (NULL, '$ma','$mb', NOW())";
+                                if (mysqli_query($connect, $sql)): endif;
+                            }
+                            mysqli_close ($connect);
+                            ?>
+                            </form>
+                    </div>
+</div>
+</p>
+
+
 </div>
 
 <!-- Link to open the modal -->
